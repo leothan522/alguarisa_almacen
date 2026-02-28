81ac2ff58dac6775b29c38309f031449501d7193
Yonathan Castillo <leothan522@gmail.com>
2026-02-28 17:20:09 -0400

    Bootstrap, Sweetlert2 con Vite

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.bootstrap')
+
+@section('title', __('Welcome')." - ". config('app.name', 'Laravel'))
+
+@section('content')
+
+    <div x-data class="text-center mb-5 mb-sm-auto">
+        @auth
+            <a class="text-muted" href="{{ route('profile.edit') }}" @click="mostrarPreloader()">{{ __('Profile') }}</a>
+            <a class="text-muted ms-3" href="{{ route('filament.dashboard.pages.dashboard') }}" @click="mostrarPreloader()">Dashboard</a>
+        @else
+            <a class="text-muted" href="{{ route('filament.dashboard.pages.dashboard') }}" @click="mostrarPreloader()">{{ __('Log in') }}</a>
+            @if (Route::has('register'))
+                <a class="text-muted ms-3" href="{{ route('register') }}" @click="mostrarPreloader()">{{ __('Register') }}</a>
+            @endif
+        @endauth
+    </div>
+
+@endsection

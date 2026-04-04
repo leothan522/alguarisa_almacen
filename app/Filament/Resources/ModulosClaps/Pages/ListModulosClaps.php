475bc0da280bfbed8fcdb55b30c816399fec6b2c
Yonathan Castillo <leothan522@gmail.com>
2026-04-04 12:22:36 -0400

    Despachar Merma en Modulos CLAP

--- app/Filament/Resources/ModulosClaps/Pages/ListModulosClaps.php
+++ app/Filament/Resources/ModulosClaps/Pages/ListModulosClaps.php
@@ -3,17 +3,22 @@
 namespace App\Filament\Resources\ModulosClaps\Pages;
 
 use App\Filament\Resources\ModulosClaps\ModulosClapResource;
+use App\Traits\MermaTrait;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListModulosClaps extends ListRecords
 {
+    use MermaTrait;
+
     protected static string $resource = ModulosClapResource::class;
 
     protected function getHeaderActions(): array
     {
+        self::$plan = 'MC';
         return [
             CreateAction::make(),
+            self::actionDespacharMerma(),
         ];
     }
 }
